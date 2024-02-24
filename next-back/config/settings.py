1d8b56a3927d386740136db3fb6e27b89f821391
kiranrokkam09 <kiran.rokkam456@gmail.com>
2024-02-24 13:45:38 +0530

    changes in settings

--- next-back/config/settings.py
+++ next-back/config/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
@@ -149,7 +149,9 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
 CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:5500",
+    "https://next-labs-chi.vercel.app/",
 ]
 
 CSRF_TRUSTED_ORIGINS = [
+    "https://next-labs-chi.vercel.app/",
 ]
